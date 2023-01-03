fcebb5eb82d3ec57c7bce5c69efdf02da182eb75
Mykola Omelianov <mykola.omelianov@smartexe.com>
2023-01-03 21:46:40 +0200

    Initial Commit

---
+++ Relax Time/Relax_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Relax_TimeApp.swift
+//  Relax Time
+//
+//  Created by Mykola Omelianov on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct Relax_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
